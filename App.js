1e87c0a579f5bda8ee80316140fc279661e47b03
Zenith <96979639+appyjuss@users.noreply.github.com>
2024-06-08 18:54:07 -0500

    loadAsync for fonts

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import NavigationStack from "./App/NavigationContainer"
+import { loadFonts } from './App/Theme';
 
 export default function App() {
-  return (
-    <NavigationStack />
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  useEffect(() => {
+    const loadApp = async () => {
+      await loadFonts();
+      setFontsLoaded(true);
+    };
+
+    loadApp();
+  }, []);
+  if (setFontsLoaded){
+    return (
+      <NavigationStack />
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
